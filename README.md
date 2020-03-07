8641904dabe5017e7ba6c1b1ce35b2e4fcdfa36a
Felipe <felipe.lucena@iteris.com.br>
2020-03-07 13:01:49 -0300

    Aula 05 - Criação da  página main,  instalação e configuração do axios e da chamada na api

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 #### Pontos interessantes:
-- utiliza yarn
-- utiliza features novas do ES6
+- yarn
+- axios
+- novas features ES6+
+
 
 #### Link do curso:
 https://skylab.rocketseat.com.br/node/curso-react-js
--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css'
 
-import Header from "./components/Header/header";
+import Header from './components/Header/header';
+import Main from './pages/main/main';
 
 const App = () => (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 
